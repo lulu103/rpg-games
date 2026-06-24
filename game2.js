aa646be3cc49375602ec44316255a1b4802a2ef4
lulu103 <andylan.mobile@gmail.com>
2026-06-24 12:58:53 +0800

    Create game2.js

---
+++ game2.js
@@ -0,0 +1,10 @@
+const game2 = {
+    name: "Coming Soon",
+    start: "start",
+    scenes: {
+        start: {
+            text: "Game 2 is not made yet.",
+            choices: []
+        }
+    }
+};
